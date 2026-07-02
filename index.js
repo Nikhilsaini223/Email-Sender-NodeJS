f7040c65beb67645ed10644e211780a4cb4c4962
Nikhilsaini223 <nikhilsaini4115@gmail.com>
2026-07-02 18:20:09 +0530

    feat: initialize email sender project with essential files and styles
    - Added package.json to manage project dependencies and scripts.
    - Created public/style.css for styling the email sending interface with a modern design.
    - Implemented views/mailpage.ejs for the email sending form layout and structure.

---
+++ index.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+const path = require("path");
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+const transporter = nodemailer.createTransport({
+  // SMTP configuration
+  host: process.env.SMTP_HOST || "smtp.gmail.com", // SMTP server host
+  port: Number(process.env.SMTP_PORT) || 587, // SMTP server port
+  secure: false, // Use TLS
+  auth: {
+    // Authentication credentials
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+app.post("/send-email", async (req, res) => {
+  const { to, subject, text } = req.body;
+
+  // const template = fs.readFile('./views/email-template.ejs')
+  // const html = ejs.render(template, {name: 'John'})
+
+  try {
+    const info = await transporter.sendMail({
+      from: '"Nikhil Saini" <memetoonstudios@gmail.com>',
+      to: to,
+      subject: subject,
+      text: text,
+      // html: '<b>Hello Message</b>',
+      // html: html,
+      attachments: [
+        {
+          filename: "data.pdf",
+          path: path.join(__dirname, "files", "data.pdf"),
+        },
+      ],
+    });
+
+    res.json({ message: "Email Send Successfully", info });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to send email", error });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.render("mailpage");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
